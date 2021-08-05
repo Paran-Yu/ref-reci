b9addda58867a17f921459fffd9430a851511549
nu1997 <jiu.1997.park@gmail.com>
2021-08-05 14:17:22 +0900

    0805 로그인화면 사진 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import SecComp from './pages/SecComp/';
 import Recipe from './pages/Recipe';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
-import FloatingActionButton from './components/Main/FloatingActionButton';
+// import FloatingActionButton from './components/Main/FloatingActionButton';
 
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route path='/reci' component={Recipe} />
         <Route path="/signIn" component={SignIn} />
         <Route path="/signup" component={SignUp} />
-        <Route path="/main" component={FloatingActionButton} />
+        {/* <Route path="/main" component={FloatingActionButton} /> */}
         <Route path="/mainref" component={MainRef} />
       </Router>
     </div>
--- frontend/src/components/Auth/SignIn/index.jsx
+++ frontend/src/components/Auth/SignIn/index.jsx
@@ -1,14 +1,12 @@
 // React, Router
 import {useState, React} from 'react';
-import { Route } from "react-router";
+// import { Route } from "react-router";
 import { BrowserRouter as Router, Link as RouterLink } from "react-router-dom";
 
 // Style
 import { makeStyles } from '@material-ui/core/styles';
 
 // Core
-import Avatar from '@material-ui/core/Avatar';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -21,12 +19,12 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 // Icons
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Background from '../../../images/authimg.png';
+import Background from '../../../images/main.png';
 
 // Server 
 import axios from 'axios';
 import server from '../../../server.json';
+
 
 const postLogin = async (url, userID, userPW) => {
     try{
@@ -51,6 +49,7 @@ const postLogin = async (url, userID, userPW) => {
     }
 }
 
+
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
@@ -71,8 +70,6 @@ const useStyles = makeStyles((theme) => ({
     },
     image: {
         // backgroundImage: `${authimg}`,
-        width: "100%",
-        height: "100%",
         backgroundImage: "url(" + Background + ")",
         // backgroundImage: 'url(https://source.unsplash.com/random)',
         backgroundRepeat: 'no-repeat',
@@ -114,13 +111,10 @@ export default function SignInSide() {
             <Grid item xs={false} sm={6} className={classes.image} />
             <Grid item xs={12} sm={6} component={Paper} elevation={6} square>
                 <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign in
+                        로그인
                     </Typography>
-                    <form className={classes.form} noValidate>
+                    <form className={classes.form}>
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -129,6 +123,7 @@ export default function SignInSide() {
                             id="email"
                             label="아이디(E-mail)"
                             name="email"
+                            type="email"
                             autoComplete="email"
                             autoFocus
                             onChange={(event) => {
@@ -158,11 +153,12 @@ export default function SignInSide() {
                                 color="primary" 
                             />
                             }
-                            label="Remember me"
+                            label="아이디 / 비밀번호 저장"
                         />
                         <Button
                             //type="submit"
                             fullWidth
+                            sizeLarge
                             variant="contained"
                             color="primary"
                             className={classes.submit}
@@ -174,46 +170,52 @@ export default function SignInSide() {
                         >
                             Sign In
                         </Button>
-                        <ButtonGroup
-                            variant="contained"
-                        >
-                            <Button
-                                xs={12}
-                                component={RouterLink}
-                                to="/#"
-                                color="yellow"
-                            >
-                                Kakao
-                            </Button>
-                            <Button
-                                xs={12}
-                                component={RouterLink}
-                                to="/#"
-                                color="blue"
-                            >
-                                Google
-                            </Button>
-                            <Button
-                                xs={12}
-                                component={RouterLink}
-                                to="/#"
-                                color="white"
-                            >
-                                GitHub
-                            </Button>
-                        </ButtonGroup>
                         <Grid container>
-                            <Grid item xs>
+                            <Grid item xs={12} sm={6}>
                                 <Link href="#" variant="body2">
                                     Forgot password?
                                 </Link>
                             </Grid>
-                            <Grid item>
+                            <Grid item xs={12} sm={6}>
                                 <Link component={RouterLink} to="/signup" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
                         </Grid>
+                        <hr></hr>
+                        <Button
+                            xs={12}
+                            mt={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="yellow"
+                            variant="contained"
+                            fullWidth
+                        >
+                            Kakao
+                        </Button>
+                        <Button
+                            xs={12}
+                            m={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="blue"
+                            variant="contained"
+                            fullWidth
+                        >
+                            Google
+                        </Button>
+                        <Button
+                            xs={12}
+                            m={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="white"
+                            variant="contained"
+                            fullWidth
+                        >
+                            GitHub
+                        </Button>
                         <Box mt={5}>
                             <Copyright />
                         </Box>
