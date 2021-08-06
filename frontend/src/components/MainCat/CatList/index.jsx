2dcc5bb799aaabca2dd3c9b0694ea7175277f9cd
강지은 <kangjieun9843@gmail.com>
2021-08-06 11:51:16 +0900

    gridChange

--- frontend/src/components/MainCat/CatList/index.jsx
+++ frontend/src/components/MainCat/CatList/index.jsx
@@ -2,6 +2,7 @@ import Ract, { useState, useEffect } from "react";
 import { Grid, makeStyles, GridList, Paper } from "@material-ui/core";
 import catDt from "./dump.json";
 import CatItem from "../CatItem";
+import { useNowCols } from "../../../common/MediaQueryHooks";
 const useGetdata = () => {
   const [catItemDatas, setCatItemDatas] = useState([]);
   const getDatas = async () => {
@@ -17,42 +18,47 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
-    width: "100%",
     justifyContent: "center",
   },
   grid: {
     border: "1px solid #dfdfdf",
     textAlign: "center",
     borderRadius: "15px",
-    margin: theme.spacing(1),
-    height: "80%",
-  },
-  list: {
-    width: "80%",
-    height: "80%",
+    margin: "auto",
   },
 }));
 
 const CatList = () => {
   const st = useStyles();
   const data = useGetdata();
+  const len = useNowCols();
+  console.log(len);
   return (
     <div className={st.root}>
-      <GridList container cols={4} className={st.list}>
-        {data.map((dt, idx) => {
-          let color;
-          if (idx == 0) {
-            color = "#F19920";
-          }
-          return (
-            <Grid item justifyContent="center" alignItems="center" key={idx} dt={dt} xs={3}>
-              <Paper className={st.grid} style={{ backgroundColor: color }}>
-                <CatItem dt={dt} />
-              </Paper>
-            </Grid>
-          );
-        })}
-      </GridList>
+      <Grid xs={10}>
+        <GridList
+          container
+          cols={Number.isInteger(len) ? len / 2 + 1 : 4}
+          style={{ height: len * 200, height: "auto", overflowY: "auto" }}
+        >
+          {data.map((dt, idx) => {
+            let color;
+            if (idx == 0) {
+              color = "#F19920";
+            }
+            return (
+              <Grid item key={idx} dt={dt} style={{ height: len * 50 + 50 }}>
+                <Paper
+                  className={st.grid}
+                  style={{ backgroundColor: color, width: len * 50 + 30, height: len * 50 + 30 }}
+                >
+                  <CatItem dt={dt} />
+                </Paper>
+              </Grid>
+            );
+          })}
+        </GridList>
+      </Grid>
     </div>
   );
 };
--- frontend/src/pages/MainRef/index.jsx
+++ frontend/src/pages/MainRef/index.jsx
@@ -6,9 +6,12 @@ import CatList from "../../components/MainCat/CatList";
 import Layout from "../../layout";
 import { ReactComponent as Logo } from "../../logo.svg";
 import SideBar from "../../components/SideBar";
+import backGround from "../../images/background.png";
 const useStyles = makeStyles((theme) => ({
   Fav: {
     margin: "auto",
+    justifyItems: "center",
+    alignItems: "center",
   },
   word: {
     color: "#A29D97",
@@ -18,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
   title: {
     color: "#45423C",
     marginTop: "60px",
+    marginBottom: "60px",
   },
 }));
 const MainRef = () => {
@@ -26,26 +30,33 @@ const MainRef = () => {
   return (
     <ThemeProvider theme={baseTheme}>
       <Layout />
-      <Grid container spacing={2} className={st.Fav}>
-        <Grid item xs={12} className={st.Fav} id={1}>
-          <Typography align="left" variant="h6" gutterBottom className={st.word}>
-            인기 폭주! 사람들이 가장 많이 찾았어요
-          </Typography>
-          <FavRecList />
+      <Grid>
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <Grid>
+            <img src={backGround} />
+          </Grid>
+          <Grid item xs={10} className={st.Fav} id={1} style={{ margin: "" }}>
+            <Typography align="left" variant="h6" gutterBottom className={st.word}>
+              인기 폭주! 사람들이 가장 많이 찾았어요
+            </Typography>
+            <FavRecList />
+          </Grid>
+          <Grid item xs={10} className={st.Fav} id={2}>
+            <Typography align="left" variant="h6" gutterBottom className={st.word}>
+              신작 레시피 도착!
+            </Typography>
+            <FavRecList />
+          </Grid>
+          <Grid item xs={12} className={st.Fav}>
+            <Typography align="center" variant="h3" gutterBottom className={st.title}>
+              나의 냉장고
+            </Typography>
+            <Grid>
+              <CatList />
+            </Grid>
+          </Grid>
+          <SideBar />
         </Grid>
-        <Grid item xs={12} className={st.Fav} id={2}>
-          <Typography align="left" variant="h6" gutterBottom className={st.word}>
-            신작 레시피 도착!
-          </Typography>
-          <FavRecList />
-        </Grid>
-        <Grid item xs={12} className={st.Fav}>
-          <Typography align="center" variant="h3" gutterBottom className={st.title}>
-            나의 냉장고
-          </Typography>
-          <CatList />
-        </Grid>
-        <SideBar />
       </Grid>
     </ThemeProvider>
   );
