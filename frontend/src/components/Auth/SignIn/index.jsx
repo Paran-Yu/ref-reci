ec24fd3417fe0126e6198aa0b370cac7f8371fd5
YDbata <kiz75@naver.com>
2021-08-09 13:59:43 +0900

    merge

--- frontend/src/components/Auth/SignIn/index.jsx
+++ frontend/src/components/Auth/SignIn/index.jsx
@@ -5,31 +5,39 @@ import { BrowserRouter as Router, Link as RouterLink } from "react-router-dom";
 
 // Style
 import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-// import { ThemeProvider } from '@material-ui/styles'
 
 // Core
 import createTheme from '@material-ui/core/styles/createTheme';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+// import { ThemeProvider } from '@material-ui/styles'
 
 // Icons
 import Background from '../../../images/main.png';
-import GitHubIcon from '@material-ui/icons/GitHub';
 
 // Server 
 import axios from 'axios';
 import server from '../../../server.json';
 
+// const kakao = createTheme({
+//     palette: {
+//         primary: {
+//             light: '#f2ede7',
+//             main: '#fee500',
+//             dark: '#191600',
+//             contrastText: '#191600',
+//         },
+//     },
+// });
 
 const mytheme = createTheme({
     palette: {
@@ -54,25 +62,6 @@ const mytheme = createTheme({
     },
 });
 
-const postSearchID = async (url, userID) => {
-    try {
-        const data = await axios({
-            method: 'post',
-            url: url,
-            data: {
-                userID: userID,
-            },
-            headers: {
-                accept: 'application/json',
-            },
-        });
-        return data.data;
-    }
-    catch (err) {
-        console.log(`ERROR: ${err}`);
-    }
-}
-
 const postLogin = async (url, userID, userPW) => {
     try{
         const data = await axios({
@@ -86,6 +75,8 @@ const postLogin = async (url, userID, userPW) => {
                 accept: 'application/json',
             },
         });
+        console.log(`url: ${url}`);
+        console.log(`data.data: ${data.data}`);
         return data.data;
     }
     catch(err){
@@ -94,11 +85,12 @@ const postLogin = async (url, userID, userPW) => {
     }
 }
 
+
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="http://i5a203.p.ssafy.io/signin">
+            <Link color="inherit" href="https://material-ui.com/">
                 Ref:reci
             </Link>{' '}
             {new Date().getFullYear()}
@@ -106,6 +98,7 @@ function Copyright() {
         </Typography>
     );
 }
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -150,29 +143,17 @@ export default function SignInSide({history}) {
         <Grid container component="main" className={classes.root}>
             <CssBaseline />
             <Grid item xs={false} sm={6} className={classes.image} />
-            <Grid item 
-                xs={12} 
-                sm={6} 
-                component={Paper} 
-                elevation={6} 
-                container
-                square
-                justifyContent="flex-start"
-                alignItems="center"
-            >
-                <ThemeProvider theme={mytheme}>
+            <Grid item xs={12} sm={6} component={Paper} elevation={6} square>
                 <div className={classes.paper}>
-                    <Typography color="primary" variant="h2">
-                        <b>Ref:Reci</b>
-                    </Typography>
-                    <br></br>
                     <Typography component="h1" variant="h5">
                         로그인
                     </Typography>
+                    
                     <form className={classes.form}>
+                    <ThemeProvider theme={mytheme}>
                         <TextField
                             variant="outlined"
-                            // margin="normal"
+                            margin="normal"
                             required
                             fullWidth
                             id="email"
@@ -197,13 +178,10 @@ export default function SignInSide({history}) {
                             autoComplete="current-password"
                             onChange={(event) => {
                                 setPassword(event.target.value);
-                                if (event.target.value.length > 20) {
-                                    alert('비밀번호는 8자 이상 20자 이하로 입력해주세요');
-                                    event.target.value = event.target.value.slice(0, -1);
-                                }
                             }}
                         />
-                        {/* <FormControlLabel
+                        
+                        <FormControlLabel
                             control={
                             <Checkbox 
                                 checked={checked}
@@ -213,74 +191,91 @@ export default function SignInSide({history}) {
                             />
                             }
                             label="아이디 / 비밀번호 저장"
-                        /> */}
+                        />
+                        
                         <Button
+                            //type="submit"
                             fullWidth
-                            size="large"
+                            sizeLarge
                             variant="contained"
                             color= "primary"
                             className={classes.submit}
                             onClick={async()=>{
                                 if(4 <= password.length && password.length <= 20){
-                                    const userDatas = await postSearchID(`${server.ip}/user/searchID`, userID);
-                                    if (userDatas.value === 'Duplicate Email') {
-                                        console.log('가입된 이메일입니다.');
-                                        const userDatas = await postLogin(`${server.ip}/user/login`, userID, password);
-                                        if (userDatas === true) {
-                                            console.log('로그인 성공');
-                                            history.push("/");
-                                        }
-                                        else {
-                                            alert('비밀번호가 틀렸습니다.');
-                                        }
+                                    const userDatas = await postLogin(`${server.ip}/user/login`, userID, password);
+
+                                    if (userDatas === true) {
+                                        console.log('로그인 성공');
+                                        history.push("/");
                                     }
                                     else {
-                                        alert('가입되지 않은 이메일입니다.');
+                                        console.log('로그인 실패');
                                     }
                                 }
                                 else{
                                     alert('비밀번호는 4자 이상, 20자 이하로 입력해주세요.')
                                     console.log(`현재 비밀번호 자릿수: ${password.length}`)
                                 }
+                                
                             }}
                         >
                             로그인
                         </Button>
                         <Grid container>
-                            <Grid item xs={6}>
-                                <Link color="secondary" component={RouterLink} to="/changepassword" variant="body2">
+                            <Grid item xs={12} sm={6}>
+                                <Link component={RouterLink} to="/changepassword" variant="body2">
                                     비밀번호 찾기
                                 </Link>
                             </Grid>
-                            <Grid item xs={6}>
-                                <Link color="secondary" component={RouterLink} to="/signup" variant="body2">
+                            <Grid item xs={12} sm={6}>
+                                <Link component={RouterLink} to="/signup" variant="body2">
                                     회원가입
                                 </Link>
                             </Grid>
                         </Grid>
-                        <br></br>
-                        <IconButton
-                            href={"https://accounts.google.com/o/oauth2/v2/auth?client_id=14050797265-gchj4gpfqu6fmdet41v1g34mc53hdoic.apps.googleusercontent.com&redirect_uri="+server.ip+"/callback/google&response_type=code&scope=profile"}
-                        >
-                            Google
-                        </IconButton>
-                        <IconButton
-                            href={"https://github.com/login/oauth/authorize?client_id=2d34711451a62f8f967d&redirect_uri="+server.ip+"/callback/github"}
-                        >
-                            <GitHubIcon />
-                        </IconButton>
-                        <IconButton
-                            href={"https://kauth.kakao.com/oauth/authorzie?client_id=c765ccaf81f7ec64ac9adacbe5f8beb7&redirect_uri="+server.ip+"/callback/kakao&response_type=code"}
+                        <hr></hr>
+                        <Button
+                            xs={12}
+                            mt={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="success"
+                            variant="contained"
+                            padding-bottom="10"
+                            fullWidth
+
                             >
                             Kakao
-                        </IconButton>
-                        <br></br>
+                        </Button>
+                        
+                        <Button
+                            xs={12}
+                            m={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="primary"
+                            variant="contained"
+                            fullWidth
+                        >
+                            Google
+                        </Button>
+                        <Button
+                            xs={12}
+                            m={2}
+                            component={RouterLink}
+                            to="/#"
+                            color="secondary"
+                            variant="contained"
+                            fullWidth
+                        >
+                            GitHub
+                        </Button>
+                        </ThemeProvider>
                         <Box mt={5}>
                             <Copyright />
                         </Box>
                     </form>
                 </div>
-                </ThemeProvider>
             </Grid>
         </Grid>
     );
