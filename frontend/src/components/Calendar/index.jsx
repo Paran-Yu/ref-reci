27382f9861c6f8bb151b268f01d78deffa8fe503
nu1997 <jiu.1997.park@gmail.com>
2021-08-10 14:43:54 +0900

    [front] folder fixed

--- frontend/src/components/Calendar/index.jsx
+++ frontend/src/components/Calendar/index.jsx
@@ -16,10 +16,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
-import server from '../../../server.json';
-import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
-import events from './event'
-import moment from 'moment'
+import server from '../../server.json';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
+import events from './event';
+import moment from 'moment';
 
 
 import "./index.css";
