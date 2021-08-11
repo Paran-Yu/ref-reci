b34d8115631e2dc67b79b1bf2684103cdd352801
맹창영 <maengchang@gmail.com>
2021-08-11 16:55:32 +0900

    today front commit

--- frontend/src/components/Calendar/Dates/index.jsx
+++ frontend/src/components/Calendar/Dates/index.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import events from './events'
 import interactionPlugin from '@fullcalendar/interaction'
 import axios from 'axios';
 import server from '../../../server.json';
@@ -24,6 +23,7 @@ const getEvents = async (url) => {
     console.log(`ERROR: ${err}`);
   }
 }
+
 
 //백에서 달 꺼를 날짜를 가져와서 캘린더에 뿌리고
 //캘린더 클릭 시 백에서 해당 날짜에 유통기한마감 상품을 다른 창에 뿌림
@@ -51,6 +51,9 @@ export default function Dates() {
           dateClick={async (info) => {
             alert(info.dateStr)
           }}
+          //foodlist로 날짜 전달
+          //달력에 선택된게 아무것도 없을 때
+          //리스트에 유효기간이 임박한 순으로 보여주기
           eventClick={(el) => {alert(el.event.startstr)}}
         />
       </section>
