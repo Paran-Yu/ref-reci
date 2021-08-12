e2479ee43f0338499f8981192e75dd66f75a22cc
맹창영 <maengchang@gmail.com>
2021-08-12 20:49:00 +0900

    Comin

--- frontend/src/components/Calendar/Dates/index.jsx
+++ frontend/src/components/Calendar/Dates/index.jsx
@@ -24,6 +24,28 @@ const getEvents = async (url) => {
   }
 }
 
+const getItems = async (url, date) => {
+  try {
+    const data = await axios({
+      method: 'post',
+      url: url,
+      data : {
+        date : date
+      },
+      headers: { 
+        accept: 'application/json'
+      }
+    })
+    return data.data
+  }
+  catch (err) {
+    console.log(url);
+    console.log(`ERROR: ${err}`);
+  }
+}
+
+
+
 
 //백에서 달 꺼를 날짜를 가져와서 캘린더에 뿌리고
 //캘린더 클릭 시 백에서 해당 날짜에 유통기한마감 상품을 다른 창에 뿌림
@@ -31,15 +53,15 @@ const getEvents = async (url) => {
 export default function Dates() {
   const calendarRef = useRef(null)
   const [calendarData, setCalendarData]=useState([])
-
-
+  const items = getItems('http://localhost:3001/foodlist/getItems',"2021-08-17")
+  console.log(items)
   useEffect(async()=>{
     const data= await getEvents('http://localhost:3001/calendar/getEvents')
     setCalendarData(data)
   },[])
   
-  console.log('캘린더')
-  console.log(typeof(calendarData), calendarData)
+  // console.log('캘린더')
+  // console.log(typeof(calendarData), calendarData)
     return(
       <section>
         <FullCalendar
