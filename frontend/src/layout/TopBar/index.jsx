7d4bb3fa6a629c0c71f382adf7ec3aa4768b50bf
unknown <dlwlgns102@naver.com>
2021-08-13 00:04:00 +0900

    update session v0.11

--- frontend/src/layout/TopBar/index.jsx
+++ frontend/src/layout/TopBar/index.jsx
@@ -90,7 +90,7 @@ export default function TopBar() {
             color="inherit" 
               onClick={async () => {
                 const data = await getLogout(`${server.ip}/user/logout`);
-                window.location.href = "http://i5a203.ssafy.p.io/signin"
+                window.location.href = "http://i5a203.p.ssafy.io/signin"
               }}>
               로그아웃
             </Button>
