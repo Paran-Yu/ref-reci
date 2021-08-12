f10530aba56f332369f75156a3b60d7ffda9413a
unknown <dlwlgns102@naver.com>
2021-08-12 11:17:15 +0900

    create userinfo backend

--- frontend/src/components/Auth/Profile/MyInfo/index.jsx
+++ frontend/src/components/Auth/Profile/MyInfo/index.jsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
-import { BrowserRouter as Router, Link as RouterLink } from "react-router-dom";
+import React, { useState } from 'react';
 
 import Typography  from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -8,8 +6,7 @@ import { Box } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-import axios from 'axios';
-import server from '../../../../server.json';
+
 
 const useStyles = makeStyles((theme) => ({
   btn: {
@@ -17,50 +14,10 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const checkLogin = async(url) => {
-  try {
-    const data = await axios({
-      method: 'get',
-      url: url,
-      withCredentials: true,
-      headers: {
-        accept: 'application/json',
-      },
-    });
-    return data.data;
-  }
-  catch (err) {
-    console.log(`ERROR: ${err}`);
-  }
-}
 
-
-
-export default function MyInfo({history}) {
+export default function MyInfo(props) {
   const classes = useStyles();
-  const [userID, setUserID] = useState('');
-  const [userName, setUserName] = useState('');
-  const [myFridgeNum, setMyFridgeNum] = useState('');
-  const [expireNum, setExpireNum] = useState('');
-
-
-  useEffect(async() => {
-    const data = await checkLogin(`${server.ip}/user/isLogin`);
-    if(data.value){
-      //필요한 데이터 가져오기
-      console.log(data.value);
-      setUserID('여기 이메일')
-      setUserName('여기 닉네임')
-    }
-    else{
-      console.log(data.value);
-      //history.replace('/signin');
-      history.push('/signin');
-      
-    }
-  },[])
-
-
+  
   return (
     <div>
       <Typography
@@ -68,12 +25,12 @@ export default function MyInfo({history}) {
         내 정보
       </Typography>
       <Box textAlign="center" p={2}>
-        <p>냉장고 속에 {myFridgeNum}개의 식재료가 있습니다.</p>
-        <p>유효기간이 3일 미만인 식재료가 {expireNum}개 있습니다.</p>
+        <p>냉장고 속에 {props.myFridgeNum}개의 식재료가 있습니다.</p>
+        <p>유효기간이 3일 미만인 식재료가 {props.expireNum}개 있습니다.</p>
       </Box>
       <Box textAlign="left">
-        <p>닉네임: {userName}</p>
-        <p>아이디(Email): {userID}</p>
+        <p>닉네임: {props.userName}</p>
+        <p>아이디(Email): {props.userID}</p>
       </Box>
       <Box p={2}>
         <div >
