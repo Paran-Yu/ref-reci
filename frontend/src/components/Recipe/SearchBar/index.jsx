dd491831f5eaaeb36ec345cecc6f720076aadf2b
unknown <dlwlgns102@naver.com>
2021-08-15 22:47:35 +0900

    create recipe search

--- frontend/src/components/Recipe/SearchBar/index.jsx
+++ frontend/src/components/Recipe/SearchBar/index.jsx
@@ -37,13 +37,16 @@ const postDatas = async (url, cl2) => {
   }
 }
 
-export default function SearchBar(props, { onChildChange }) {
+export default function SearchBar(props) {
   const classes = useStyles();
   const [selectedOptions, setSelectedOptions] = useState([]);
   
 
   const handleChange = async (event, value) => {
     setSelectedOptions(value);
+
+    console.log(event);
+    console.log(value);
   
     const len = value.length;
     let selectedSet = new Set();
@@ -57,11 +60,8 @@ export default function SearchBar(props, { onChildChange }) {
     const recipes = await postDatas(`${server.ip}/recipe/search`, selectedArr)
 
     //부모에서 내려준 함수로  레시피 아이디들, 선택된 재료의 소분류를 넘김
-    // console.log(recipes);
-    console.log('부모에게 넘겨주기 시작')
-    onChildChange(recipes, selectedArr);
-    console.log('부모에게 넘겨주기 완료')
-
+    props.onChildChange(recipes, selectedArr);
+    
   }
   
   const allFoodItems = props.datas;
@@ -88,19 +88,3 @@ export default function SearchBar(props, { onChildChange }) {
     </div>
   );
 }
-
-// const allFoodItems = [
-//   { name: '멋쟁이 토마토', category: '과일'},
-//   { name: '쌀국수 밀키트', category: '인스턴트'},
-//   { name: '유기농 유정란', category: '계란'},
-//   { name: 'CJ가 직접 끓인 사골국', category: '인스턴트'},
-//   { name: '데이터 좀 넣자', category: '과일'},
-//   { name: '내가 먹고 싶은 음식', category: '과일'},
-//   { name: '지금은 배불러서...', category: '과일'},
-//   { name: '유부초밥', category: '과일'},
-//   { name: '맛있는 피자', category: '과일'},
-//   { name: '싱싱한 사과', category: '과일'},
-//   { name: '내 맘대로 만드는 초콜렛', category: '과일'},
-//   { name: '청정원 케찹', category: '과일'},
-//   { name: '다양한 상품이름', category: '과일'},
-// ];
