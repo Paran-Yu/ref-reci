8c873ca881af4080082f5c75c350bb75bc79e0b4
ParkJiwoo <jiu___@naver.com>
2021-08-16 22:19:08 +0900

    [front] update recipe detail page

--- frontend/src/pages/RecipeDetail/index.jsx
+++ frontend/src/pages/RecipeDetail/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Container, Box, Typography, Paper, Grid, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
-import TopBar from '../../../layout/TopBar';
-import BottomBar from '../../../layout/BottomBar';
-import FloatingActionButton from '../../../layout/FloatingActionButton';
-import RecipeTitle from '../../../components/RecipeDetail/RecipeTitle';
-import RecipeContent from '../../../components/RecipeDetail/RecipeContent';
+import TopBar from '../../layout/TopBar';
+import BottomBar from '../../layout/BottomBar';
+import FloatingActionButton from '../../layout/FloatingActionButton';
+import RecipeTitle from '../../components/RecipeDetail/RecipeTitle';
+import RecipeContent from '../../components/RecipeDetail/RecipeContent';
 
 
 const useStyles = makeStyles((theme) => ({
