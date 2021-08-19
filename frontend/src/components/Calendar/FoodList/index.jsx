a6552e8d0e9d1efbf5f41a5df64393b3e18a6ac5
ParkJiwoo <jiu___@naver.com>
2021-08-20 06:29:06 +0900

    [front] debug

--- frontend/src/components/Calendar/FoodList/index.jsx
+++ frontend/src/components/Calendar/FoodList/index.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
